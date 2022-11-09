93d49d5765024435f7e0360fac01edbe60494433
vasilvas99 <ivanovvasil99@gmail.com>
2022-11-09 14:28:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# Info dump
+
+A script that SSH's to a host an obtains relevant system information.
+
+## Usage
+
+To setup the SSH credentials and other connection information edit the `dump_info_config.json` file.
+
+```json
+{
+    "HOST": "192.168.7.1", # host ip address/hostname
+    "PORT": "22", # ssh port
+    "SSH_USER": "root", # ssh login username
+    "SSH_PASS": "", # password
+    "PING_TIMES": 1, # number of times the host would be ping-ed before ssh connection is attempted
+    "DUMP_TO_FILE": "yes" # should the scraped info be dumped to a file  - <yes/no>
+}
+```
+
+This script does not depend on a system installation of OpenSSH but rather uses the `paramiko` library that is a "pure-Python" implenetation of the ssh protocol.
+
+To install it:
+
+`pip3 install paramiko`
+
+Other than paramiko, there are no external dependencies.
+
+
+## Running
+
+After paramiko is installed and the json config file is approprietly edited please use:
+
+`python dump_info.py`
+
+## Example output:
+
+```json
+{
+  "board_info": [
+    "Hardware = BCM2835",
+    "Revision = d03114",
+    "Serial = 100000007c38a9ed",
+    "Model = Raspberry Pi 4 Model B Rev 1.4"
+  ],
+  "uname": "Linux 5.10.83-v8 aarch64",
+  "os_release": [
+    "BUILD_ID=\"20221107145736\"",
+    "ID=leda",
+    "IMAGE_VERSION=\"v0.0.23-95-gdd83f39\"",
+    "NAME=\"Eclipse Leda\"",
+    "PRETTY_NAME=\"Eclipse Leda 2022 (Hockenheim)\"",
+    "VERSION=\"2022 (Hockenheim)\"",
+    "VERSION_CODENAME=\"Hockenheim\"",
+    "VERSION_ID=v0.0.23-95-gdd83f39"
+  ],
+  "hostname": "leda-c939",
+  "eth0_MAC": "e4:5f:01:0e:c9:39",
+  "eth0_IPv4": "192.168.7.1",
+  "wlan_MAC": "e4:5f:01:0e:c9:3b",
+  "wlan0_IPv4": ""
+}
+```
---
+++ dump_info.py
@@ -0,0 +1,93 @@
+import paramiko
+import subprocess
+import platform
+import sys
+import json
+
+# Load config
+with open("dump_info_config.json", "r") as json_fh:
+    conf = json.load(json_fh)
+
+HOST = conf["HOST"]
+PORT = conf["PORT"]
+USERNAME = conf["SSH_USER"]
+PASSWORD = conf["SSH_PASS"]
+PING_TIMES = conf["PING_TIMES"]
+DUMP_TO_FILE = "y" in conf["DUMP_TO_FILE"].lower()
+
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+
+def ping(host, retries=1):
+    parameter = "-n" if platform.system().lower() == "windows" else "-c"
+
+    command = ["ping", parameter, str(retries), host]
+    response = subprocess.call(command)
+
+    if response != 0:
+        raise ConnectionError(
+            f"Could not ping PI on ip {host}. Are you sure your PC network interface is configured correctly?"
+        )
+
+
+try:
+    print(f"Trying to ping RPI {PING_TIMES} time(s)....")
+    ping(HOST, retries=PING_TIMES)
+    print("Success!")
+except ConnectionError as ce:
+    eprint(ce)
+    exit(-1)
+
+client = paramiko.client.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+try:
+    print("Trying username-password based authentication")
+    client.connect(
+        HOST,
+        port=PORT,
+        username=USERNAME,
+        password=PASSWORD,
+        look_for_keys=False,
+        allow_agent=False,
+    )
+except Exception as ex:
+    eprint("Username-Password authentication failed. Exiting...")
+    exit(-1)
+
+print("=============================================")
+print("Successful SSH authentication!")
+print("=============================================")
+
+
+def execute_single_command(bash_cmd: str):
+    _, stdout, _ = client.exec_command(bash_cmd)
+    return stdout.read().decode("ascii", "ignore").strip()
+
+
+info_dict = {}
+info_dict["board_info"] = (
+    execute_single_command(r"cat /proc/cpuinfo | tail -n 4")
+    .replace("\t", "")
+    .replace(":", " =")
+    .split("\n")
+)
+info_dict["uname"] = execute_single_command(r"echo $(uname) $(uname -r) $(uname -p)")
+info_dict["os_release"] = execute_single_command(r"cat /usr/lib/os-release").split("\n")
+info_dict["hostname"] = execute_single_command(r"cat /etc/hostname")
+info_dict["eth0_MAC"] = execute_single_command(r"cat /sys/class/net/eth0/address")
+info_dict["eth0_IPv4"] = execute_single_command(r"ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'")
+info_dict["wlan_MAC"] = execute_single_command(r"cat /sys/class/net/wlan0/address")
+info_dict["wlan0_IPv4"] = execute_single_command(r"ip -4 addr show wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'")
+
+client.close()
+print("=============================================")
+print("Configuration obtained")
+print("=============================================")
+print(json.dumps(info_dict, indent=2))
+
+if DUMP_TO_FILE:
+    with open("system_info.json", "w") as fh:
+        json.dump(info_dict, fh, indent=2)
